d3a5716d2d07fd776f90cb3e834b6107094c8239
malcolm worthy <malworthy@gmail.com>
2022-08-29 20:29:30 +1000

    14.6

--- src/chunk.c
+++ src/chunk.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 
 #include "chunk.h"
+#include "memory.h"
 
 void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte)
@@ -18,4 +20,18 @@ void writeChunk(Chunk* chunk, uint8_t byte)
     }
     chunk->code[chunk->count] = byte;
     chunk->count++;
+}
+
+void freeChunk(Chunk* chunk)
+{
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
+    initChunk(chunk);
+}
+
+int addConstant(Chunk* chunk, Value value)
+{
+    writeValueArray(&chunk->constants, value);
+
+    return chunk->constants.count-1;
 }
--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main (int argc, const char* argv[])
 {
     Chunk chunk;
 
     initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
 
-    char *x = "hello, world\n";
-    printf(x);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, constant);
+
+    disassembleChunk(&chunk, "test chunk");
+
+    freeChunk(&chunk);
 
     return 0;
 }
