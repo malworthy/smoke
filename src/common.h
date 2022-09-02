b2d090723fe128e6046ce3a05f9e7e755fcea1ee
malcolm worthy <malworthy@gmail.com>
2022-09-02 17:04:36 +1000

    16.4

--- src/common.h
+++ src/common.h
@@ -1,10 +1,10 @@
 #ifndef min_common_h
 #define min_common_h
 
+#define DEBUG_TRACE_EXECUTION
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-
 
 #endif
--- src/debug.c
+++ src/debug.c
@@ -31,7 +31,7 @@ static int constantInstruction(const char* name, Chunk* chunk,
 int disassembleInstruction(Chunk* chunk, int offset) 
 {
     printf("%04d ", offset);
-    
+
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) 
     {
         printf("   | ");
@@ -48,6 +48,16 @@ int disassembleInstruction(Chunk* chunk, int offset)
             return simpleInstruction("OP_RETURN", offset);
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_ADD:
+            return simpleInstruction("OP_ADD", offset);
+        case OP_SUBTRACT:
+            return simpleInstruction("OP_SUBTRACT", offset);
+        case OP_MULTIPLY:
+            return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_DIVIDE:
+            return simpleInstruction("OP_DIVIDE", offset);
+        case OP_NEGATE:
+            return simpleInstruction("OP_NEGATE", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
