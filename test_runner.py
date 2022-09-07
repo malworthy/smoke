541e3c28c25def3f52f814d5ccf5f3e822ce00da
malworthy <malworthy@gmail.com>
2022-09-07 16:34:22 +1000

    Add files via upload

---
+++ test_runner.py
@@ -0,0 +1,43 @@
+import subprocess
+import sys
+
+def test_file(interpreter, script_to_test, expect):
+    print(f"Running test: {script_to_test}")
+    file = open(script_to_test,'r')
+    contents = file.readlines()
+
+    expected = [x.replace(expect, "").strip() for x in contents if x.startswith(expect)]
+    if len(expected) == 0:
+        print("No expected result provided. Test aborted")
+        return 1
+
+    result = subprocess.run([interpreter, script_to_test], capture_output=True)
+    actual = result.stdout.decode().split()
+
+    if len(actual) != len(expected):
+        print(f"FAIL: Expect: {expected}, Actual: {actual}")
+        return 1
+
+    i = 0
+    for line in actual:
+        if expected[i] != line:
+            print(f"FAIL: Expect '{expected[i]}', Actual '{line}'")
+            return 1
+        i += 1
+
+    print("PASS")
+    return 0
+
+# ENTRY POINT #
+args = sys.argv
+
+if len(args) != 4:
+    print("Usage: test_runner.py [interpeter] [scripts to test] [expected result prefix]")
+    exit_code = 64
+else:
+    interpreter = args[1]
+    script_to_test = args[2]
+    expect = args[3]
+    exit_code = test_file(interpreter, script_to_test, expect)
+
+sys.exit(exit_code)
