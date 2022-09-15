14c6fe7ff5667abcb15e524b221f78e82376aa5f
malcolm worthy <malworthy@gmail.com>
2022-09-15 19:49:39 +1000

    24.4

--- src/compiler.c
+++ src/compiler.c
@@ -46,7 +46,15 @@ typedef struct {
     bool constant;
 } Local;
 
-typedef struct {
+typedef enum {
+    TYPE_FUNCTION,
+    TYPE_SCRIPT
+} FunctionType;
+
+typedef struct Compiler {
+    struct Compiler* enclosing;
+    ObjFunction* function;
+    FunctionType type;
     Local locals[UINT8_COUNT];
     int localCount;
     int scopeDepth;
@@ -64,7 +72,7 @@ static void declaration();
 
 static Chunk* currentChunk() 
 {
-    return compilingChunk;
+    return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) 
@@ -199,23 +207,43 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) 
+static void initCompiler(Compiler* compiler, FunctionType type) 
 {
+    compiler->enclosing = current;
+    compiler->function = NULL;
+    compiler->type = type;
     compiler->localCount = 0;
     compiler->scopeDepth = 0;
+    compiler->function = newFunction();
     current = compiler;
+
+    if (type != TYPE_SCRIPT) 
+    {
+        current->function->name = copyString(parser.previous.start,
+                                            parser.previous.length);
+    }
+
+    Local* local = &current->locals[current->localCount++];
+    local->depth = 0;
+    local->name.start = "";
+    local->name.length = 0;
 }
 
-static void endCompiler() 
+static ObjFunction* endCompiler() 
 {
     emitReturn();
+    ObjFunction* function = current->function;
 
 #ifdef DEBUG_PRINT_CODE
     if (!parser.hadError) 
     {
-        disassembleChunk(currentChunk(), "code");
+        disassembleChunk(currentChunk(), function->name != NULL
+        ? function->name->chars : "<script>");
     }
 #endif
+
+    current = current->enclosing;
+    return function;
 }
 
 static void beginScope() 
@@ -534,6 +562,25 @@ static void block()
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
+static void markInitialized() 
+{
+    if (current->scopeDepth == 0) return;
+
+    current->locals[current->localCount - 1].depth =
+      current->scopeDepth;
+}
+
+static void defineVariable(uint8_t global) 
+{
+    if (current->scopeDepth > 0) 
+    {
+        markInitialized();
+        return;
+    }
+    
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static uint8_t parseVariable(const char* errorMessage, bool isConst) 
 {
     consume(TOKEN_IDENTIFIER, errorMessage);
@@ -548,21 +595,41 @@ static uint8_t parseVariable(const char* errorMessage, bool isConst)
     return identifierConstant(&parser.previous);
 }
 
-static void markInitialized() 
+static void function(FunctionType type) 
 {
-  current->locals[current->localCount - 1].depth =
-      current->scopeDepth;
+    Compiler compiler;
+    initCompiler(&compiler, type);
+    beginScope(); 
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+    if (!check(TOKEN_RIGHT_PAREN)) 
+    {
+        do 
+        {
+            current->function->arity++;
+            if (current->function->arity > 255) 
+                errorAtCurrent("Can't have more than 255 parameters.");
+            
+            uint8_t constant = parseVariable("Expect parameter name.", false);
+            defineVariable(constant);
+        } while (match(TOKEN_COMMA));
+    }
+
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+    block();
+
+    ObjFunction* function = endCompiler();
+    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
 }
 
-static void defineVariable(uint8_t global) 
+static void funDeclaration() 
 {
-    if (current->scopeDepth > 0) 
-    {
-        markInitialized();
-        return;
-    }
-    
-    emitBytes(OP_DEFINE_GLOBAL, global);
+    uint8_t global = parseVariable("Expect function name.", true);
+    markInitialized();
+    function(TYPE_FUNCTION);
+    defineVariable(global);
 }
 
 static void loopVarDeclaration() 
@@ -713,7 +780,10 @@ static void synchronize()
 static void declaration() 
 {
     bool isConst = match(TOKEN_CONST);
-    if (isConst || match(TOKEN_VAR)) 
+
+    if (match(TOKEN_FN)) 
+        funDeclaration();
+    else if (isConst || match(TOKEN_VAR)) 
         varDeclaration(isConst);
     else 
         statement();
@@ -741,12 +811,12 @@ static void statement()
         expressionStatement();
 }
 
-bool compile(const char* source, Chunk* chunk) 
+ObjFunction* compile(const char* source)
 {
     initScanner(source);
     Compiler compiler;
-    initCompiler(&compiler);
-    compilingChunk = chunk;
+    initCompiler(&compiler, TYPE_SCRIPT);
+
     parser.hadError = false;
     parser.panicMode = false;
     advance();
@@ -754,6 +824,7 @@ bool compile(const char* source, Chunk* chunk)
     {
         declaration();
     }
-    endCompiler();
-    return !parser.hadError;
+    
+    ObjFunction* function = endCompiler();
+    return parser.hadError ? NULL : function;
 }
